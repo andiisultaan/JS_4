23d601868e63234b86d7c908fac4a2bf095e5681
andiisultaan <71545557+andiisultaan@users.noreply.github.com>
2024-01-07 09:35:39 +0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,26 @@
+let contacts = [{
+name: "Maxwell Wright",
+phone: "(0191) 719 6495",
+email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+}, {
+name: "Raja Villarreal",
+phone: "0866 398 2895",
+email: "posuere.vulputate@sed.com"
+}, {
+name: "Helen Richards",
+phone: "0800 1111",
+email: "libero@convallis.edu"
+}, {
+name: "Maisie Haley",
+phone: "0913 531 3030",
+email: "risus.Quisque@urna.ca"
+}];
+
+console.log("Contact 1 : ")
+console.log(contacts[0].name, "/", contacts[0].phone, "/", contacts[0].email);
+console.log("Contact 2 : ")
+console.log(contacts[1].name, "/", contacts[1].phone, "/", contacts[1].email);
+console.log("Contact 3 : ")
+console.log(contacts[2].name, "/", contacts[2].phone, "/", contacts[2].email);
+console.log("Contact 4 : ")
+console.log(contacts[3].name, "/", contacts[3].phone, "/", contacts[3].email);
---
+++ function.js
@@ -0,0 +1,27 @@
+function contact1() {
+    let name = "Maxwell Wright";
+    let phone = "(0191) 719 6495";
+    let Email = "Curabitur.egestas.nunc@nonummyac.co.uk";
+    console.log("Contact 1 : ");
+    console.log(name, "/", phone, "/", Email);
+}
+
+function contact2() {
+    let name = "Raja Villarreal";
+    let phone = "0866 398 2895";
+    let Email = "posuere.vulputate@sed.com";
+    console.log("Contact 2 : ");
+    console.log(name, "/", phone, "/", Email);
+}
+
+function contact3() {
+    let name = "Helen Richards";
+    let phone = "0800 1111";
+    let Email = "libero@convallis.edu";
+    console.log("Contact 3 : ");
+    console.log(name, "/", phone, "/", Email);
+}
+
+contact1();
+contact2();
+contact3();
